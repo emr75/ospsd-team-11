4e4d33f486fe319c4b1993b7cb16b8ad1613d97b
zilid10 <zili.ding101@gmail.com>
2026-04-22 14:11:31 -0400

    Setup terraform

---
+++ infra/outputs.tf
@@ -0,0 +1,11 @@
+output "service_id" {
+  value = render_web_service.app.id
+}
+
+output "service_name" {
+  value = render_web_service.app.name
+}
+
+output "service_url" {
+  value = render_web_service.app.url
+}
